849879a04aa215f0be00145287a80914a0a87a92
Danny Love <feldhaus@cecs.pdx.edu>
2024-02-27 20:04:29 -0800

    Fixed __init__ import

--- src/NumberLang/__init__.py
+++ src/NumberLang/__init__.py
@@ -1,6 +1,6 @@
-import binary
-from device import device
-from listener import Listener
-from speaker import initialize_speaker, map_scribe_to_speaker
-from scribe import Scribe
-from training import train
+from . import binary
+from .device import device
+from .listener import Listener
+from .speaker import initialize_speaker, map_scribe_to_speaker
+from .scribe import Scribe
+from .training import train
